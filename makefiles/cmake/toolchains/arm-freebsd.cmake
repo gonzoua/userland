6bbcadb60334b2ca9ff2cca770f3079f6c8cd5bb
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2015-01-05 08:33:40 -0800

    Increase default stack size, 4K is not enough

--- makefiles/cmake/toolchains/arm-freebsd.cmake
+++ makefiles/cmake/toolchains/arm-freebsd.cmake
@@ -11,6 +11,7 @@ SET(CMAKE_SYSTEM_PROCESSOR arm)
 
 #ADD_DEFINITIONS("-march=armv6")
 add_definitions("-mcpu=arm1176jzf-s -mfpu=vfp")
+add_definitions("-DVCOS_DEFAULT_STACK_SIZE=16384")
 
 # rdynamic means the backtrace should work
 IF (CMAKE_BUILD_TYPE MATCHES "Debug")
